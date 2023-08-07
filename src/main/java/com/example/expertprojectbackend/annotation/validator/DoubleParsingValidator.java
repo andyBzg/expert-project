182ae109d0621ff908ca6e160702c9bddbfd886e
luxorious <vovan_194@mail.ru>
2023-08-07 21:33:35 +0200

    create add validation

---
+++ src/main/java/com/example/expertprojectbackend/annotation/validator/DoubleParsingValidator.java
@@ -0,0 +1,25 @@
+package com.example.expertprojectbackend.annotation.validator;
+
+import com.example.expertprojectbackend.annotation.DoubleValidator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class DoubleParsingValidator implements ConstraintValidator<DoubleValidator, String> {
+    @Override
+    public void initialize(DoubleValidator constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if (s == null) {
+            return false;
+        }
+            try {
+                return Double.parseDouble(s)<0;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+    }
+}
