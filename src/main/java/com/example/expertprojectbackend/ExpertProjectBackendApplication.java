95a46e4c72fec0894558580a0151b24322c670b9
luxorious <vovan_194@mail.ru>
2023-08-07 21:21:38 +0200

    create project

---
+++ src/main/java/com/example/expertprojectbackend/ExpertProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.expertprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/expertprojectbackend/ExpertProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expertprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
