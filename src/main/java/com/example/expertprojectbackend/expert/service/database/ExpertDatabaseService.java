f7b4d97d783d5d51d89bb75cc9bd94f52799581a
andyBzg <andr.bzg@gmail.com>
2023-08-29 22:41:05 +0200

    add expert database service

---
+++ src/main/java/com/example/expertprojectbackend/expert/service/database/ExpertDatabaseService.java
@@ -0,0 +1,8 @@
+package com.example.expertprojectbackend.expert.service.database;
+
+import com.example.expertprojectbackend.expert.dto.ExpertDto;
+
+public interface ExpertDatabaseService {
+
+    void create(ExpertDto expertDto);
+}
---
+++ src/main/java/com/example/expertprojectbackend/expert/service/database/impl/ExpertDatabaseServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.expertprojectbackend.expert.service.database.impl;
+
+import com.example.expertprojectbackend.expert.dto.ExpertDto;
+import com.example.expertprojectbackend.expert.service.database.ExpertDatabaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExpertDatabaseServiceImpl implements ExpertDatabaseService {
+    @Override
+    public void create(ExpertDto expertDto) {
+        // TODO method logic
+    }
+}
