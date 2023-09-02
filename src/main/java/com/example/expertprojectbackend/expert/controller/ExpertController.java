b499cdccddede0ebb086c0b456ad490eebd71365
andyBzg <andr.bzg@gmail.com>
2023-09-02 11:32:16 +0200

    Add 'changePassword' endpoint to ExpertController

--- src/main/java/com/example/expertprojectbackend/expert/controller/ExpertController.java
+++ src/main/java/com/example/expertprojectbackend/expert/controller/ExpertController.java
@@ -1,7 +1,10 @@
 package com.example.expertprojectbackend.expert.controller;
 
-import com.example.expertprojectbackend.expert.dto.ExpertDto;
+import com.example.expertprojectbackend.shared.dto.ChangePasswordDto;
+import com.example.expertprojectbackend.expert.dto.ExpertRegistrationDto;
 import com.example.expertprojectbackend.expert.service.database.ExpertDatabaseService;
+import com.example.expertprojectbackend.shared.service.CredentialsService;
+import com.example.expertprojectbackend.shared.util.session.CurrentSessionUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExpertController {
 
     private final ExpertDatabaseService expertDatabaseService;
+    private final CredentialsService credentialsService;
+    private final CurrentSessionUtil currentSessionUtil;
 
     @PostMapping(value = "/registration")
-    public ResponseEntity<ExpertDto> registerExpert(@RequestBody ExpertDto expertDto) {
-        expertDatabaseService.create(expertDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(expertDto);
+    public ResponseEntity<ExpertRegistrationDto> registerExpert(@RequestBody ExpertRegistrationDto registrationDto) {
+        expertDatabaseService.registerNewExpert(registrationDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registrationDto);
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordDto changePasswordDto) {
+        String currentUsername = currentSessionUtil.getCurrentUsername();
+        credentialsService.changePassword(currentUsername, changePasswordDto);
+        return ResponseEntity.ok().build();
     }
 }
