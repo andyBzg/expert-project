51854ae05a4b9ffb8334e0cee7c7f0ce5ac26a48
andyBzg <andr.bzg@gmail.com>
2023-09-02 11:27:49 +0200

    add new data transfer objects

---
+++ src/main/java/com/example/expertprojectbackend/client/dto/ClientRegistrationDto.java
@@ -0,0 +1,9 @@
+package com.example.expertprojectbackend.client.dto;
+
+import lombok.Data;
+
+@Data
+public class ClientRegistrationDto {
+    private String email;
+    private String password;
+}
