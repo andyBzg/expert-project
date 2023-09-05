ed62b46487780c8be2f1cfe1afe86a1451998681
andyBzg <andr.bzg@gmail.com>
2023-09-05 21:46:30 +0200

    add client database service interface and implementation, client registration implementation

--- src/main/java/com/example/expertprojectbackend/client/service/ClientDatabaseService.java
+++ src/main/java/com/example/expertprojectbackend/client/service/ClientDatabaseService.java
@@ -1,4 +1,8 @@
 package com.example.expertprojectbackend.client.service;
 
+import com.example.expertprojectbackend.client.dto.ClientRegistrationDto;
+
 public interface ClientDatabaseService {
+    void registerNewClient(ClientRegistrationDto registrationDto);
+
 }
