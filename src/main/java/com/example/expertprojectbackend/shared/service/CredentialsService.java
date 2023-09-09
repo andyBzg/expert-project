9210ff71fd66290d531a77eb831f3b30aed04bdd
andyBzg <andr.bzg@gmail.com>
2023-09-09 21:07:59 +0200

    refactor

--- src/main/java/com/example/expertprojectbackend/shared/service/CredentialsService.java
+++ src/main/java/com/example/expertprojectbackend/shared/service/CredentialsService.java
@@ -4,4 +4,6 @@ import com.example.expertprojectbackend.shared.dto.ChangePasswordDto;
 
 public interface CredentialsService {
     void changePassword(String currentUsername, ChangePasswordDto changePasswordDto);
+
+    void registerCredentials(String email, String encodedPassword, String role);
 }
