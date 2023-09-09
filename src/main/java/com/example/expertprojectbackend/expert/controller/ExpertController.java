1f1ee750c96b30441765e86061052262c737ad83
andyBzg <andr.bzg@gmail.com>
2023-09-09 20:49:34 +0200

    refactor endpoints and request matchers

--- src/main/java/com/example/expertprojectbackend/expert/controller/ExpertController.java
+++ src/main/java/com/example/expertprojectbackend/expert/controller/ExpertController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/expert")
+@RequestMapping("/api/expert")
 @RequiredArgsConstructor
 public class ExpertController {
 
