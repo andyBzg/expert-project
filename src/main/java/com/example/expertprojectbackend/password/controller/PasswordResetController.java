3ff7f6067daf1b494e1f56756de616b2bff700c1
andyBzg <andr.bzg@gmail.com>
2023-09-23 12:27:12 +0200

    feat: Added forgotten password restore functionality
    - Added password reset request and new password confirmation DTOs
    - Added password reset token, controller, service and repository
    - Added exceptions caused due password reset workflow and exception handlers

---
+++ src/main/java/com/example/expertprojectbackend/password/controller/PasswordResetController.java
@@ -0,0 +1,42 @@
+package com.example.expertprojectbackend.password.controller;
+
+import com.example.expertprojectbackend.password.dto.PasswordResetDto;
+import com.example.expertprojectbackend.password.dto.PasswordResetRequestDto;
+import com.example.expertprojectbackend.password.service.PasswordResetService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/password-reset")
+@RequiredArgsConstructor
+@Slf4j
+public class PasswordResetController {
+
+    private final PasswordResetService passwordResetService;
+
+    @PostMapping("/request")
+    public ResponseEntity<String> requestPasswordReset(
+            @RequestBody PasswordResetRequestDto resetRequestDto, final HttpServletRequest request) {
+        log.info("Request password reset");
+        passwordResetService.requestPasswordReset(resetRequestDto, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/validate-token")
+    public ResponseEntity<String> validateResetToken(@RequestParam("token") String token) {
+        log.info("Validating password reset token");
+        passwordResetService.validateResetToken(token);
+        return ResponseEntity.ok("Token is valid.");
+    }
+
+    @PostMapping("/reset")
+    public ResponseEntity<String> resetPassword(
+            @RequestParam("token") String token, @RequestBody PasswordResetDto passwordResetDto) {
+        log.info("Resetting password");
+        passwordResetService.resetPassword(token, passwordResetDto);
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/com/example/expertprojectbackend/shared/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/expertprojectbackend/shared/exception_handler/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.example.expertprojectbackend.shared.exception_handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,5 +21,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleBadCredentialsException() {
         log.error("Incorrect credentials");
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<String> handleDisabledException() {
+        log.error("User is disabled");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 }
